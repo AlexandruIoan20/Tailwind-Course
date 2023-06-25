a40abde1188457ca050b214bba4c9d132a6ca787
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-06-25 14:36:45 +0300

    Hook darkmode

--- fireship-course/components/NavBar.jsx
+++ fireship-course/components/NavBar.jsx
@@ -1,8 +1,39 @@
+'use client'; 
+
+import { FaSun, FaMoon } from 'react-icons/fa'; 
+import { useEffect } from 'react';
+
 import SideBar from "./SideBar"
+import useDarkMode from "@hooks/useDarkMode"
+
+const ThemeHandler = () => { 
+    const [darkTheme, setDarkTheme] = useDarkMode(); 
+    const handleMode = () => setDarkTheme(!darkTheme); 
+
+    useEffect( () => { 
+        console.log({ darkTheme }); 
+    }, [darkTheme])
+
+    return ( 
+        <span className='fixed top-0 right-0 bg-color-green top-navigation' onClick = { handleMode }>
+            { darkTheme ? 
+                ( 
+                    <FaSun size = '24' className = 'top-navigation-icon' /> 
+                ) : 
+                ( 
+                    <FaMoon size = '24' className='top-navigation-icon' /> 
+                )
+            }
+        </span>
+    )
+}
 
 const NavBar = () => {
   return (
-    <SideBar /> 
+    <nav>
+        <ThemeHandler />
+        <SideBar /> 
+    </nav>
   )
 }
 
--- fireship-course/components/SideBar.jsx
+++ fireship-course/components/SideBar.jsx
@@ -14,7 +14,7 @@ const SideBarIcon = ({ icon, text = 'tooltip' }) => {
 
 const SideBar = () => {
   return (
-    <div className='fixed top-0 left-0 h-screen w-16 flex flex-col bg-primary text-white shadow-lg'>
+    <div className='fixed top-0 left-0 w-16 flex flex-col bg-primary text-white shadow-lg dark:bg-white dark:text-black'>
         <SideBarIcon icon = { <FaFire size ='28' /> } /> 
         <SideBarIcon icon = { <BsPlus size ='32' /> } /> 
         <SideBarIcon icon = { <BsFillLightningFill size ='20' /> } /> 
